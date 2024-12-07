0c0ff85fec9c66a4bff96c2b58c060e18f967209
mahmoud.eid13 <eid.mahmoud1312@gmail.com>
2024-12-07 15:39:28 +0200

    installing redux and creating the user and cart slices

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,5 +1,9 @@
+import { useSelector } from "react-redux";
 import CreateUser from "../features/user/CreateUser";
+import Button from "./Button";
 function Home() {
+  const username = useSelector((state) => state.user.username);
+
   return (
     <div className="text-center my-10 sm:my-16 px-4">
       <h1 className=" text-xl font-semibold mb-8 md:text-3xl">
@@ -9,7 +13,13 @@ function Home() {
           Straight out of the oven, straight to you.
         </span>
       </h1>
-      <CreateUser />
+      {username === "" ? (
+        <CreateUser />
+      ) : (
+        <Button to={"./menu"} type="primary">
+          Continue ordering, {username}
+        </Button>
+      )}
     </div>
   );
 }
